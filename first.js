a40f2f1793cb23eb26b371c41558797455a1f52c
Jonas Katauskis <katauskisjonas@gmail.com>
2016-01-15 14:45:47 +0200

    Try out of JSON

--- first.js
+++ first.js
@@ -1,35 +1,37 @@
+/* global printColors */
 
-//Try out of Ajax
+//Try out of JSON
 $(document).ready(function(){
     $(".ajax").on("click", function() {
-    $.get("data.txt", function( response ) {
-        console.log( response );
-        });
-    });	
-
-function printColors(colorPalettes){
-for(var palette in colorPalettes){
-console.log(colorPalettes[palette].title);
-console.log(colorPalettes[palette].colors[0]); //HEX color code
-}
-}
+    $.getJSON("http://www.colourlovers.com/api/palettes/top?jsonCallback=?,",
+    { numResults: 3 },
+    function(colorPalettes) {
+    printColors(colorPalettes);
+    });
+    function printColors(colorPalettes){
+        for(var palette in colorPalettes){
+        console.log(colorPalettes[palette].title);
+        console.log(colorPalettes[palette].colors[0]); //HEX color code
+        };
+    };	
+});
 // Form script
 	$(".btn").on("click", function (event) {
         event.preventDefault();
-		var userName = $(this).closest("form").find(".userName").val();
-		var email = $(this).closest("form").find(".email").val();
-		var password = $(this).closest("form").find(".password").val();
+		var userName = $(this).closest("form").find("#text").val();
+		var email = $(this).closest("form").find("#email").val();
+		var password = $(this).closest("form").find("#pwd").val();
 		if(userName == null || userName == ""){
 			alert("Name must be filled out");
-			return false;
+			return;
 		} 
 		if(email == null || email == ""){
 			alert("Email must be filled out");
-			return false;
+			return;
 		}
 		if(password == null || password == ""){
 			alert("Password must be filled out");
-			return false;
+			return;
 		} 
 		$(".successful").fadeIn();
         $(".form-background").slideDown();
